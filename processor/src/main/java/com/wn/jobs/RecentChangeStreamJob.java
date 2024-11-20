72877f8502573213ca981a3db8bda0e9cc0a14f0
Stefan <stefan.rvic@gmail.com>
2024-11-20 01:25:47 +0100

    imp flink job with mongodb sink

--- processor/src/main/java/com/wn/jobs/RecentChangeStreamJob.java
+++ processor/src/main/java/com/wn/jobs/RecentChangeStreamJob.java
@@ -1,5 +1,6 @@
 package com.wn.jobs;
 
+import com.mongodb.client.model.InsertOneModel;
 import com.wn.models.RecentChange;
 import com.wn.operators.LogFunction;
 import com.wn.operators.WindowedCounter;
@@ -7,10 +8,14 @@ import com.wn.serde.RecentChangeDeserializer;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.connector.mongodb.sink.MongoSink;
+import org.apache.flink.connector.mongodb.sink.writer.serializer.MongoSerializationSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.bson.Document;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class RecentChangeStreamJob {
 
@@ -25,6 +30,20 @@ public class RecentChangeStreamJob {
 				.setValueOnlyDeserializer(new RecentChangeDeserializer())
 				.build();
 
+		MongoSink<Long> sink = MongoSink.<Long>builder()
+				.setUri("mongodb://admin:adminpassword@localhost:27017/admin?authSource=admin")
+				.setDatabase("wiki_changes")
+				.setCollection("COUNT")
+				.setSerializationSchema(
+						(MongoSerializationSchema<Long>) (element, sinkContext) -> {
+							Document doc = new Document()
+									.append("timestamp", LocalDateTime.now().toString())
+									.append("count", element)
+									.append("window_size_seconds", 10);
+
+							return new InsertOneModel<>(doc.toBsonDocument());})
+				.build();
+
 		env
 				.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
 				.map(new LogFunction<RecentChange>().setGenerator(rc -> String.format("received changes from page : %s", rc.getMeta().getUri())))
@@ -32,8 +51,8 @@ public class RecentChangeStreamJob {
 				.window(TumblingProcessingTimeWindows.of(Duration.ofSeconds(10)))
 				.apply(new WindowedCounter())
 				.map(new LogFunction<Long>().setGenerator(count -> String.format("counted %d changes in 10 seconds", count)))
-				.print();
+				.sinkTo(sink);
 
-		env.execute("Simple Kafka Consumer");
+		env.execute("Wikipedia Change Stream Job");
 	}
 }
